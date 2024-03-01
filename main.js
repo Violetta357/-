b83ea435b6a2e10e1d58ed0868b05bb0388618f4
Violetta357 <pampidyy@mail.ru>
2024-03-01 15:47:39 +0300

    hw2.7-game

--- main.js
+++ main.js
@@ -14,10 +14,34 @@ const findSeason = (a) => {
   } else {
     return "Осень";
   }
-
 };
 const firstGame = () => {
-    alert ('Готов играть ?')
-    let monthNomber = Number(prompt('Введите порядковый номер месяца')) 
-    alert (findSeason(monthNomber))
-}
+  alert("Готов играть ?");
+  let monthNomber = Number(prompt("Введите порядковый номер месяца"));
+  alert(findSeason(monthNomber));
+};
+
+const fruitGames = () => {
+  const fruits = [
+    "Яблоко",
+    "Груша",
+    "Дыня",
+    "Виноград",
+    "Персик",
+    "Апельсин",
+    "Мандарин",
+  ];
+  alert(fruits.sort(() => Math.random() - 0.5));
+  let firstFruit = prompt("Какой фрукт был первым в списке?");
+  let lastFruit = prompt("Какой фрукт был последним в списке?");
+  if (firstFruit == fruits[0] && lastFruit == fruits[fruits.length - 1]) {
+    return alert("Поздравляю! Верный ответ!");
+  } else if (
+    firstFruit === fruits[0] ||
+    lastFruit === fruits[fruits.length - 1]
+  ) {
+    return alert("Вы были близки к победе!!");
+  } else {
+    return alert("Вы не угадали.");
+  }
+};
